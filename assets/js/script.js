56230156b328cc4d51e4134318633aa3b098fdfa
Diab <emailcodydiab@gmail.com>
2020-04-15 09:02:18 -0500

    added drag and drop edit and delete

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,7 +73,7 @@ tasks[status][index].text = text;
  saveTasks();
 
  var taskP =$("<p>")
-  .addCLass("m-1")
+  .addClass("m-1")
   .text(text);
 
   $(this).replaceWith(taskP);
@@ -121,7 +121,71 @@ $(".list-group").on("blur", "input[type='text']",function(){
  })
 
 })
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  active: function(event) {
+    console.log("deactivate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate",this);
+  },
+  over:function(event) {
+    console.log("over", event.target)
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    var tempArr = [];
+   $(this).children().each(function(){
+     var text = $(this)
+     .find("p")
+     .text()
+     .trim();
 
+     var date = $(this)
+     .find("span")
+     .text()
+     .trim();
+
+     tempArr.push({
+       text: text,
+       date: date
+     });
+
+    
+   });
+
+   var arrName = $(this)
+   .attr("id")
+   .replace("list-", "");
+
+   tasks[arrName] = tempArr;
+   saveTasks();
+
+  }
+
+
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui){
+   ui.draggable.remove();
+
+  },
+  over: function(event,ui) {
+    console.log("over");
+
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+})
 
 
 
